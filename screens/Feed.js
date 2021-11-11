1b4f6e8d8f1da154a634fcaedc2c4d9cbae741cf
Anish Pandey <approxap@gmail.com>
2021-11-11 09:39:23 -0500

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Feed extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Here's the Feed!</Text>
+            </View>
+        );
+    }
+}
